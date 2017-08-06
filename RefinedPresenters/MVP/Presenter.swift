241bad520f8d181ba7876ac079be2482b3f2e1b5
Brandon Evans <brandon@brandonevans.ca>
2017-08-06 09:03:52 -0600

    Extract template method into new DataFetching protocol

--- RefinedPresenters/MVP/Presenter.swift
+++ RefinedPresenters/MVP/Presenter.swift
@@ -7,18 +7,7 @@ public protocol Presenter: class {
     func teardownView()
 }
 
-/// A refinement of Presenter for the common case where:
-///  - it manages a single view
-///  - it uses a single type of data
-///  - it loads that data, potentially asynchronously
-///
-/// For this cases, implementing FetchingPresenter means that the only work to be done is specifying what data to load (usually delegated to a Service) and how to transform the data for display in the View.
-public protocol FetchingPresenter: Presenter {
-    associatedtype View: WaitingView
-    var view: View? { get }
-    associatedtype Wireframe: AlertPresenting
-    var wireframe: Wireframe { get }
-
+public protocol DataFetching: class {
     associatedtype Data
     var currentTask: CancellablePromise<Data>? { get set }
 
@@ -30,7 +19,10 @@ public protocol FetchingPresenter: Presenter {
     func dataFailedToLoad(error: Error)
 }
 
-extension FetchingPresenter {
+public extension DataFetching {
+    func preLoad() {}
+    func postLoad() {}
+
     /// Template method that lays out the process of loading data
     public func loadData() {
         if let task = currentTask {
@@ -50,8 +42,27 @@ extension FetchingPresenter {
             self.postLoad()
         }
     }
+}
 
+/// A refinement of Presenter for the common case where:
+///  - it manages a single view
+///  - it uses a single type of data
+///  - it loads that data, potentially asynchronously
+///
+/// For this case, implementing FetchingPresenter means that the only work to be done is specifying what data to load (usually delegated to a Service) and how to transform the data for display in the View.
+public protocol FetchingPresenter: Presenter, DataFetching {
+    associatedtype View: WaitingView
+    var view: View? { get }
+    associatedtype Wireframe: AlertPresenting
+    var wireframe: Wireframe { get }
+}
+
+extension FetchingPresenter {
     // MARK: Default implementations that take advantage of the constraints on View and Wireframe
+
+    public func initializeView() {
+        loadData()
+    }
 
     public func preLoad() {
         view?.wait()
